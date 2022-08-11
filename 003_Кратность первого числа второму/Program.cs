34e6e82234b0fb8ea99851fa5e983109d9d68529
Di <dimas2343@mail.ru>
2022-08-11 15:23:50 +0300

    remeinderOfDivision = num1 % num2

--- 003_Кратность первого числа второму/Program.cs
+++ 003_Кратность первого числа второму/Program.cs
@@ -2,12 +2,24 @@
 // является ли первое число кратным второму. Если число 1 не кратно числу 2,
 // то программа выводит остаток от деления.
 
-Console.WriteLine("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+//  int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 == num2 * num2)
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num1 = ReadInt("Введите первое число: ");
+int num2 = ReadInt("Введите второе сисло: ");
+
+
+ int remeinderOfDivision = num1 % num2;
+
+if (remeinderOfDivision == 0)
 {
     Console.WriteLine(num1 + " кратно " + num2);
 }
@@ -16,3 +28,4 @@ else
     int ostatok = num1 % num2;
     Console.WriteLine("остаток от деления " + ostatok);
 }
+
