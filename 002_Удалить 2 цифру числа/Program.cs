82b4264223f48f6b449af22354e6e6d08858c0d9
Di <dimas2343@mail.ru>
2022-08-11 13:17:37 +0300

    123 delSecDigit 13

---
+++ 002_Удалить 2 цифру числа/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+
+int randomNumber = GetRandomNumber(100, 999);
+Console.WriteLine(randomNumber);
+
+
+
+// int firstDigit = randomNumber / 100;
+// int lastDigit = randomNumber % 10;
+
+// Console.Write(firstDigit * 10 + lastDigit);
+ 
+int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max+1);
+}
+
+int DelSecNum (int num)
+ {
+    int firstDigit = randomNumber / 100;
+    int lastDigit = randomNumber % 10;
+    return firstDigit * 10 + lastDigit;
+} 
+
+Console.Write(DelSecNum(randomNumber));
---
+++ 003_Кратность первого числа второму/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли первое число кратным второму. Если число 1 не кратно числу 2,
+// то программа выводит остаток от деления.
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine(num1 + " кратно " + num2);
+}
+else
+{
+    int ostatok = num1 % num2;
+    Console.WriteLine("остаток от деления " + ostatok);
+}
