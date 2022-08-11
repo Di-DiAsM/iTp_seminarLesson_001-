a8d01c841ed1ff25c2f2de326b1d7e75098232c7
Di <dimas2343@mail.ru>
2022-08-11 05:19:09 +0300

    max digit in number

---
+++ 001_Наибольшая цифра числа/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int randomNumber = GetRandomNumber(10,99);
+Console.WriteLine(randomNumber);
+
+int firstDigit = randomNumber / 10;
+int lastDigit = randomNumber % 10;
+
+int maxDigit = Max(firstDigit, lastDigit);
+
+Console.WriteLine(maxDigit);
+
+int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max + 1);
+}
+
+int Max(int a, int b)
+{
+    int max = 0;
+    if (a > b)
+    {
+        max = a;
+    }
+    else
+    {
+        max = b;
+    }
+    return max;
+}
